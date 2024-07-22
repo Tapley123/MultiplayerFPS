ae3c81d839e18e2df229f22ff67e521599e54141
Tapley123 <conortapley1@gmail.com>
2024-07-23 00:46:07 +0100

    Test RPC for sending data

--- Multiplayer FPS/Assets/1_Scripts/CachedData.cs
+++ Multiplayer FPS/Assets/1_Scripts/CachedData.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Xml.Serialization;
 
 public class CachedData : MonoBehaviour
 {
