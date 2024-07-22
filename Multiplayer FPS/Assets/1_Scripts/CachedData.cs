2d628f454fde5cb590e421bbaa418fed99b59710
Tapley123 <conortapley1@gmail.com>
2024-07-22 23:57:30 +0100

    Test Cached Data

---
+++ Multiplayer FPS/Assets/1_Scripts/CachedData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CachedData : MonoBehaviour
+{
+    #region Singleton
+    private static CachedData _instance;
+    public static CachedData Instance { get { if (_instance == null) { _instance = GameObject.FindObjectOfType<CachedData>(); } return _instance; } }
+    #endregion
+
+    public int testInt;
+
+    private void Awake()
+    {
+        //Persist through scenes
+        DontDestroyOnLoad(this);
+
+        //Makes sure there is only one instance of this class
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+    }
+}
